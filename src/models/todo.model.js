d50128f69f33b6adda92b96c5de851f112cd7c5f
Martin Macharia <mosmartin5@gmail.com>
2020-04-21 18:55:18 +0200

    Create a Todo Model
    Create the Todo model.

---
+++ src/models/todo.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  done: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;
