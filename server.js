6fa321bcad883a0b1dd4cd753b8bce8b845f12c7
Martin Macharia <mosmartin5@gmail.com>
2020-04-21 17:01:05 +0200

    Create the server
    Create the Express server.

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server running on port 3000");
+});
