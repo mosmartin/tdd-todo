45d1a573c55501268689eb90003875bc9c8e804a
Martin Macharia <mosmartin5@gmail.com>
2020-04-23 18:59:50 +0200

    Add an Integration Test to check error message.
    Create an Intergration Test for checking that the error message is a JSON object.

--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -4,12 +4,24 @@ const newTodo = require("../mock/new-todo.json");
 
 const endpointUrl = "/todos";
 
-describe(endpointUrl, () => {
-  it("POST " + endpointUrl, async () => {
+describe(`POST ${endpointUrl}`, () => {
+  it("should create a todo", async () => {
     const response = await request(app).post(endpointUrl).send(newTodo);
 
     expect(response.statusCode).toBe(201);
     expect(response.body.title).toBe(newTodo.title);
     expect(response.body.done).toBe(newTodo.done);
+  });
+
+  it("should return error code 500 on malformed data with POST", async () => {
+    const response = await request(app)
+      .post(endpointUrl)
+      .send({ title: "Missing done property" });
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({
+      message:
+        "ValidationError: Todo validation failed: done: Path `done` is required.",
+    });
   });
 });
